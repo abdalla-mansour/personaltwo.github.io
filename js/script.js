03f82f4b2904ef6f72db605cee3e86f9178d8803
abdallamansour <abdallamansour@gmail.com>
2022-09-11 15:39:04 +0200

    upload Files

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Common reveal options to create reveal animations
+ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+// Target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.aside .logo', { delay: 900, origin: 'top' });
+ScrollReveal().reveal('.aside .nav', { delay: 700, origin: 'left' });
+
+ScrollReveal().reveal('.home .hello', { delay: 900, origin: 'top' });
+ScrollReveal().reveal('.home .my-profession', { delay: 1200, origin: 'right' });
+ScrollReveal().reveal('.home p', { delay: 800, origin: 'left' });
+ScrollReveal().reveal('.home .row', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.home .home-img', { delay: 900, origin: 'bottom' });
+
+ScrollReveal().reveal('.section-title', { delay: 600, origin: 'top' });
+
+ScrollReveal().reveal('.about .about-text h3', { delay: 800, origin: 'left' });
+ScrollReveal().reveal('.about .about-text p', { delay: 1000, origin: 'right' });
+ScrollReveal().reveal('.about .info-item', { delay: 900, origin: 'left' });
+ScrollReveal().reveal('.about .buttons', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.about .skills-item', { delay: 900, origin: 'right' });
+ScrollReveal().reveal('.about .education', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.about .experience', { delay: 600, origin: 'bottom' });
+
+ScrollReveal().reveal('.service .service-item', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.service .service-item .icon', { delay: 900, origin: 'top' });
+ScrollReveal().reveal('.service .service-item h4', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.service .service-item p', { delay: 1200, origin: 'top' });
+
+ScrollReveal().reveal('.portfolio .portfolio-heading', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.portfolio .portfolio-item', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.portfolio .portfolio-img .img-one', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.portfolio .portfolio-img .img-two', { delay: 900, origin: 'bottom' });
+ScrollReveal().reveal('.portfolio .portfolio-img .img-three', { delay: 1100, origin: 'bottom' });
+ScrollReveal().reveal('.portfolio .portfolio-img .img-four', { delay: 1300, origin: 'bottom' });
+ScrollReveal().reveal('.portfolio .portfolio-img .img-five', { delay: 1500, origin: 'bottom' });
+ScrollReveal().reveal('.portfolio .portfolio-img .img-six', { delay: 1700, origin: 'bottom' });
+
+ScrollReveal().reveal('.contact .contact-title', { delay: 900, origin: 'top' });
+ScrollReveal().reveal('.contact .contact-sub-title', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.contact .contact-info-item .icon', { delay: 900, origin: 'top' });
+ScrollReveal().reveal('.contact .contact-info-item h4', { delay: 700, origin: 'left' });
+ScrollReveal().reveal('.contact .contact-info-item p', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.contact .contact-form', { delay: 1000, origin: 'bottom' });
